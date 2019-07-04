93b6af363864822c69021c142abedc046ea97c41
JLCodeSource <JLCodeSource@gmail.com>
2019-07-04 16:53:26 +0100

    commit - added Floyd's score

--- server.go
+++ server.go
@@ -5,6 +5,19 @@ import (
 	"fmt"
 )
 
+// PlayerServer is a http server that returns the player score
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := r.URL.Path[len("/players/"):]
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+
+	
 }
