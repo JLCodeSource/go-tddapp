09cac1381bee60cd7004a78999b1aa480cdf156b
JLCodeSource <JLCodeSource@gmail.com>
2019-07-04 19:17:27 +0100

    Fixed integration test glitch

--- server_integration_test.go
+++ server_integration_test.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"testing"
-//	"net/http"
-//	"net/http/httptest"
+	"net/http"
+	"net/http/httptest"
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+	player := "Pepper"
 
+	for i:=0; i<3; i++ {
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	}
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	
+	assertStatus(t, response.Code, http.StatusOK)
+	assertResponseBody(t, response.Body.String(), "3")
 }
