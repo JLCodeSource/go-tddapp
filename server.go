4a3046c2a99a38c4bacbda8fbbf94f0b5e2cb30d
JLCodeSource <JLCodeSource@gmail.com>
2019-07-04 17:18:09 +0100

    Added StubPlayerStore

--- server.go
+++ server.go
@@ -5,21 +5,17 @@ import (
 	"fmt"
 )
 
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
 // PlayerServer is a http server that returns the player score
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-
-	fmt.Fprint(w, GetPlayerScore(player))
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
-
-	if name == "Floyd" {
-		return "10"
-	}
-
-	return ""
-}
