3b0aa2173c112c4950e6dc750482cff72ba224a0
JLCodeSource <JLCodeSource@gmail.com>
2019-07-04 18:02:08 +0100

    Refactor to split out routing, show & win

--- server.go
+++ server.go
@@ -17,15 +17,20 @@ type PlayerServer struct {
 }
 
 
-// PlayerServer is a http server that returns the player score
+// PlayerServer is a http server that provides the http.Method routing
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
 	
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
-	
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -33,6 +38,10 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	fmt.Fprint(w, score)
-	
+
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+		w.WriteHeader(http.StatusAccepted)
 }
 
