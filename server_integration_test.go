1c5f1d357a979b4f24d229a5bf91dece2f72afb1
JLCodeSource <JLCodeSource@gmail.com>
2019-07-05 16:12:37 +0100

    Refactored test for multiple players

--- server_integration_test.go
+++ server_integration_test.go
@@ -15,6 +15,7 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	}
 
+
 	t.Run("get score", func(t *testing.T) {
 
 		response := httptest.NewRecorder()
@@ -23,6 +24,13 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "3")
 	})
+
+	player = "Bob"
+
+	for i:=0; i<5; i++ {
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	}
+
 	t.Run("get league", func(t *testing.T) {
 
 		response := httptest.NewRecorder()
@@ -33,6 +41,7 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		want := []Player{
 			{"Pepper", 3},
+			{"Bob", 5},
 		}
 		assertLeague(t, got, want)
 	})
