c20d2a6dfc670282b58adbcdf5f6ed8aa1fef2bf
JLCodeSource <JLCodeSource@gmail.com>
2019-07-10 18:46:17 +0100

    Implemented assertScheduledAlert

--- cli_test.go
+++ cli_test.go
@@ -10,18 +10,21 @@ import (
 
 var dummySpyAlerter = &SpyBlindAlerter{}
 
+type scheduledAlert struct {
+	at time.Duration
+	amount int
+}
+
+func (s scheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.amount, s.at)
+}
+
 type SpyBlindAlerter struct {
-	alerts []struct{
-		scheduledAt time.Duration
-		amount int
-	}
+	alerts []scheduledAlert
 }
 
 func (s *SpyBlindAlerter) ScheduledAlertAt(duration time.Duration, amount int) {
-	s.alerts = append(s.alerts, struct {
-		scheduledAt time.Duration
-		amount int
-	}{duration, amount})
+	s.alerts = append(s.alerts, scheduledAlert{duration, amount})
 }
 
 func TestCLI(t *testing.T) {
@@ -57,10 +60,7 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		cases := []struct{
-			expectedScheduleTime time.Duration
-			expectedAmount	int
-		} {
+		cases := []scheduledAlert {
 			{0 * time.Second, 100},
 			{10 * time.Minute, 200},
 			{20 * time.Minute, 300},
@@ -74,26 +74,24 @@ func TestCLI(t *testing.T) {
 			{100 * time.Minute, 8000},
 		}
 
-		for i, c := range cases {
-			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), 
-				func(t *testing.T) {
+		for i, want := range cases {
+			t.Run(fmt.Sprintf(want.String()), func(t *testing.T) {
 					if len(blindAlerter.alerts) <= i {
 						t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
 					}
 
-					alert := blindAlerter.alerts[i]
+					got := blindAlerter.alerts[i]
 
-					amountgot := alert.amount
-					if amountgot != c.expectedAmount {
-						t.Errorf("got amount %d, want %d", amountgot, c.expectedAmount)
-					}
-
-					gotScheduledTime := alert.scheduledAt
-
-					if gotScheduledTime != c.expectedScheduleTime {
-						t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
-					}
+					assertScheduledAlert(t, got, want)
 				})
 		}
 	})
+}
+
+func assertScheduledAlert(t *testing.T, got, want scheduledAlert) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
